f72ad6a1c9b8a3081b4cd4bc6d8c33f268584488
lAngel <angelcanal97@gmail.com>
2022-05-09 18:57:24 +0200

    [4] HU4: Seguridad

---
+++ src/main/java/es/unizar/tmdad/config/CORSConfig.java
@@ -0,0 +1,21 @@
+package es.unizar.tmdad.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig {
+
+
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
